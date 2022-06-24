7d00bc558030b494804961110ffba5aefb5bc4fe
Matheus <matheus_501@hotmail.com>
2022-06-24 19:33:11 -0300

    trabalhando com mutation graphql

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,8 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4oet2gg25pz01z24osj018c/master',
+  uri: import.meta.env.VITE_API_URL,
+
+  headers: { Authorization: `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}` },
   cache: new InMemoryCache(),
 });
